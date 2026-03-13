a2871d8b91528b4feaa73d93d4a7af23ab954443
jesusguilhermesenateles <jesusguilhermesenateles@gmail.com>
2026-03-13 14:45:26 -0300

    update projeto

--- Primeiro_projt_Sena/obj/Debug/net10.0/Primeiro_projt_Sena.AssemblyInfo.cs
+++ Primeiro_projt_Sena/obj/Debug/net10.0/Primeiro_projt_Sena.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primeiro_projt_Sena")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82082f04833775ea1b259f184b0011f348ea1f03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primeiro_projt_Sena")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primeiro_projt_Sena")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
