26e147972d6a1e7a2b1c6c99062c3eb754d846a0
jesusguilhermesenateles <jesusguilhermesenateles@gmail.com>
2026-03-18 17:58:33 -0300

    updade Action Teste, resultado

--- Primeiro_projt_Sena/Controllers/AlunoController.cs
+++ Primeiro_projt_Sena/Controllers/AlunoController.cs
@@ -4,11 +4,25 @@ namespace Primeiro_projt_Sena.Controllers
 {
     public class AlunoController : Controller
     {
+        public object TempDataData { get; private set; }
+
         public IActionResult Index()
         {
             ViewBag.nota = "Aluno nota 10";
             ViewData["Escola"] = "E.E. Anhanguera";
             return View();
+        }
+        public IActionResult Teste()
+        {
+            TempData["MSG_EX"] = "Mensagem do TempData";
+            return RedirectToAction("Resultado");
+        }
+        public IActionResult Resultado()
+        {
+            if (TempData["MSG_EX"] != null)
+                ViewBag.Exemplo = "Aluno ETEC MB";
+            return View();
+
         }
     }
 }
--- Primeiro_projt_Sena/obj/Debug/net10.0/Primeiro_projt_Sena.AssemblyInfo.cs
+++ Primeiro_projt_Sena/obj/Debug/net10.0/Primeiro_projt_Sena.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primeiro_projt_Sena")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2871d8b91528b4feaa73d93d4a7af23ab954443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e4d7196ee2cb021ae5c3872973ee5797196340")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primeiro_projt_Sena")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primeiro_projt_Sena")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
